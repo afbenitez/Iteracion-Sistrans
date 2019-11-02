5a7babc2092d4855257e3503916ffe501bc7522f
Miguel Ramos <ma.ramosh@sis.virtual.uniandes.edu.co>
2019-11-02 09:28:58 -0500

    Arreglos

---
+++ src/main/java/uniandes/isis2304/parranderos/persistencia/PersistenciaEps.java
@@ -0,0 +1,10 @@
+package uniandes.isis2304.parranderos.persistencia;
+
+import org.apache.log4j.Logger;
+
+public class PersistenciaEps {
+
+	private static Logger log = Logger.getLogger(PersistenciaEps.class.getName());
+
+	
+}
